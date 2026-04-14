f04a6e943ab6104e9389641eec150eee8caccf0b
Kislaya N <kislaya345@gmail.com>
2026-04-14 13:47:47 +0530

    refactor: fixed minor issues

--- eda/eda.py
+++ eda/eda.py
@@ -11,41 +11,42 @@ class EDA:
         self.feature_columns = data[0].columns
         self.skewness_stats = None
         self.outliers_stats_df = None
-        self.corr_pairs = None
     
     def analyze_skew(self):
         # Select only numerical columns
-        num_cols = self.dataframe_X.select_dtypes(include=['int64', 'float64']).columns
+        num_df = self.dataframe_X.select_dtypes(include=[np.number])
         
-        skewness = {}
+        skew_series = num_df.skew()
         
-        for col in num_cols:
-            skew_value = self.dataframe_X[col].skew()
+        skew_df = skew_series.to_frame(name='skew_value')
+        skew_df = skew_df.dropna()
             
+        def classify(val):
             # classify skew
-            if abs(skew_value) > 1:
-                level = "Highly Skewed"
-            elif abs(skew_value) > 0.5:
-                level = "Moderately Skewed"
+            if abs(val) > 1:
+                return "Highly Skewed"
+            elif abs(val) > 0.5:
+                return "Moderately Skewed"
             else:
-                level = "Normal"
+                return "Normal"
             
-            skewness[col] = {
-                "skew_value": round(skew_value, 3),
-                "type": level
-            }
+        skew_df['type'] = skew_df['skew_value'].apply(classify)
         
-        self.skewness_stats = pd.DataFrame(skewness).T
+        skew_df['skew_value'] = skew_df['skew_value'].round(3)
         
-        return self.skewness_stats
+        self.skewness_stats = skew_df
+        return skew_df
     
     def analyze_outliers(self):
         results = {}
         
-        num_cols = self.dataframe_X.select_dtypes(include=['int64', 'float64']).columns
+        num_df = self.dataframe_X.select_dtypes(include=[np.number])
         
-        for col in num_cols:
+        for col in num_df.columns:
             series = self.dataframe_X[col].dropna()
+            
+            if len(series) == 0:
+                continue
             
             Q3 = series.quantile(0.75)
             Q1 = series.quantile(0.25)
@@ -68,142 +69,52 @@ class EDA:
         
             results[col] = {
                     "outlier_%": round(percentage, 2),
-                    "level": level
+                    "level": level,
+                    "Upper_boundary": upper,
+                    "Lower_boundary": lower
                 }
     
-        self.outliers_stats_df = pd.DataFrame(results).T
+        outliers_df = pd.DataFrame(results).T
+        self.outliers_stats_df = outliers_df
     
-        return self.outliers_stats_df
-    
-    def analyze_corr(self, threshold=0.8):
-        # Used for detecting linear relationships in the dataset 
-        
-        num_df = self.dataframe_X.select_dtypes(include=['int64', 'float64'])
-        
-        corr_matrix = num_df.corr().abs()
-        
-        mask = np.triu(np.ones(corr_matrix.shape), k = 1).astype(bool)
-        
-        upper = corr_matrix.where(mask)
-        
-        pairs = upper.stack().reset_index()
-        pairs.columns = ['feature-1','feature-2', 'correlation']
-        
-        pairs = pairs[pairs['correlation'] > threshold]
-        pairs = pairs.sort_values(by='correlation', ascending=False)
-        
-        self.corr_pairs = pairs 
-        
-        return self.corr_pairs
-    
-    def features_to_drop(self, threshold=0.8):
-        X_dataframe = self.dataframe_X.select_dtypes(include=['int64','float64'])
-        
-        corr_matrix = X_dataframe.corr().abs()
-        target_corr = X_dataframe.corrwith(self.dataframe_y).abs()
-        
-        upper = corr_matrix.where(np.triu(np.ones(corr_matrix.shape), k=1).astype(bool))
-        
-        to_drop = set()
-        
-        for col in upper.columns:
-            for row in upper.index:
-                if upper.loc[row, col] > threshold:
-                    
-                    # compare with target
-                    
-                    if target_corr[row] < target_corr[col]:
-                        to_drop.add(row)
-                    else: 
-                        to_drop.add(col)
-        
-        return list(to_drop)
+        return outliers_df
     
     def analyze_cardinality(self):
-        self.categoric_cols = self.dataframe_X.select_dtypes(include=['bool', 'object']).columns.tolist()
+        cat_cols = self.dataframe_X.select_dtypes(
+            include=['bool', 'object', 'category']
+        ).columns.tolist()
         
         results = {}
         
-        if self.categoric_cols:
-            for col in self.categoric_cols:
-                unique_count = self.dataframe_X[col].nunique()
-                ratio = unique_count / len(self.dataframe_X)
+        for col in cat_cols:
+            series = self.dataframe_X[col]
+            
+            unique_count = series.nunique(drop_na=True)
+            total = len(series)
+            ratio = unique_count / len(series) if total > 0 else 0
                 
-                if ratio > 0.9:
-                    level = "ID like"
-                    suggestion = "Drop these column"
-                
-                elif ratio > 0.2:
-                    level = "Very High Cardinality"
-                    suggestion = "Use target/Frequency encoding"
-                    
-                elif ratio > 0.1:
-                    level = "High Cardinality"
-                    suggestion = "Group Rare Categories"
-                    
-                elif ratio > 0.01:
-                    level = "Moderate Cardinality"
-                    suggestion = "OHE"
-                    
-                else: 
-                    level = "Low Cardinality"
-                    suggestion = "OHE"
-                    
-                results[col] = {
+            if ratio > 0.9 and unique_count > 50:
+                level = "ID like"
+            elif unique_count > 30:
+                level = "Very High Cardinality"
+            elif unique_count > 10:
+                level = "High Cardinality" 
+            elif unique_count > 3:
+                level = "Moderate Cardinality"
+            else: 
+                level = "Low Cardinality"
+               
+            results[col] = {
                     "Unique": unique_count,
                     "Ratio": round(ratio, 3),
                     "Cardinality": level,
-                    "Suggestion": suggestion
                 }
             
-            card_df = pd.DataFrame(results).T
-            
-            id_cols = card_df[card_df['Ratio'] > 0.9].index.tolist()
-
-            low_card_cols = card_df[card_df['Ratio'] <= 0.1].index.tolist()
-
-            high_card_cols = card_df[
-                (card_df['Ratio'] > 0.1) & (card_df['Ratio'] <= 0.9)
-            ].index.tolist()
+        card_df = pd.DataFrame(results).T if results else pd.DataFrame()
         
-        if not self.categoric_cols:
-            return [], [], []
-            
-        return low_card_cols, high_card_cols, id_cols
+        self.cardinality_stats = card_df
         
-    def analyze_vif(self, threshold = 10):
-        
-        X_dataframe = self.dataframe_X.copy()
-        
-        X_dataframe = X_dataframe.select_dtypes(include=['int64', 'float64'])
-        X_dataframe = X_dataframe.fillna(X_dataframe.median())
-        
-        dropped_cols = []
-        
-        while True:
-            vif_data = pd.DataFrame()
-            vif_data['Features'] = X_dataframe.columns
-            
-            vif_data['VIF'] = [
-                variance_inflation_factor(X_dataframe.values, i)
-                for i in range(X_dataframe.shape[1])
-            ]
-            
-            max_vif = vif_data["VIF"].max()
-            
-            if max_vif < threshold:
-                break
-            
-            drop_feature = vif_data.sort_values(by='VIF', ascending=False)['Features'].iloc[0]
-            dropped_cols.append(drop_feature)
-            
-            X_dataframe = X_dataframe.drop(columns=[drop_feature])
-            
-        self.vif_result = vif_data.sort_values(by='VIF', ascending=False)
-        self.vif_selected_features = X_dataframe.columns.tolist()
-        self.vif_dropped_features = dropped_cols
-            
-        return self.vif_result, self.vif_selected_features
+        return card_df
     
     def get_skewed_cols(self):
         skew_df = self.analyze_skew()
@@ -217,23 +128,13 @@ class EDA:
     
     def build_preprocessing_plan(self):
         lowcard, highcard, idcard = self.analyze_cardinality()
-        _, vif_cols = self.analyze_vif()
-        
         skew_cols = self.get_skewed_cols()
-        log_cols = skew_cols
-        
         outlier_cols = self.get_outlier_cols()
-        robust_cols  =[col for col in outlier_cols if col not in log_cols]
+        
         return {
-            "Log Transform": log_cols,
-            "Robust Scale": robust_cols,
-            "Standard Scale": [
-                col for col in self.dataframe_X.select_dtypes(include=['int64', 'float64']).columns
-                if col not in log_cols + robust_cols 
-            ],
-            "OHE (One Hot Encoding)": lowcard,
-            "High Card Encode": highcard,
-            "Drop Corr": self.features_to_drop(),
-            "Drop ID": idcard,
-            "VIF Selected": vif_cols 
+            "skewed_cols": skew_cols,
+            "outlier_cols": outlier_cols,
+            "cow_card_cols": lowcard,
+            "high_card_cols": highcard,
+            "id_cols": idcard,
         }
